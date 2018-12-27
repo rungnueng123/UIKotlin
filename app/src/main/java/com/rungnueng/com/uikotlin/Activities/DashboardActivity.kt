5c95d126976f574825bfea76db28d9683a3de97c
RUNGNUENG <rungnueng19@hotmail.com>
2018-12-27 11:11:01 +0700

    Slide Image

--- app/src/main/java/com/rungnueng/com/uikotlin/Activities/DashboardActivity.kt
+++ app/src/main/java/com/rungnueng/com/uikotlin/Activities/DashboardActivity.kt
@@ -1,7 +1,9 @@
 package com.rungnueng.com.uikotlin.Activities
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -9,11 +11,18 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.rungnueng.com.uikotlin.Adapters.SlideImageAdapter
+import com.rungnueng.com.uikotlin.Model.SlideImageModel
 import com.rungnueng.com.uikotlin.R
+import com.viewpagerindicator.CirclePageIndicator
+import java.util.*
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var mDrawerLayout: DrawerLayout
+
+    private var imageModelArrayList: ArrayList<SlideImageModel>? = null
+    private val myImageList = intArrayOf(R.drawable.harley2, R.drawable.benz2, R.drawable.vecto, R.drawable.webshots, R.drawable.bikess, R.drawable.img1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +39,68 @@ class DashboardActivity : AppCompatActivity() {
         mDrawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        imageModelArrayList = ArrayList()
+        imageModelArrayList = populateList()
+
+        initImageSlide()
+
+    }
+
+    private fun populateList(): ArrayList<SlideImageModel>? {
+        val list = ArrayList<SlideImageModel>()
+
+        for (i in 0..5) {
+            val slideImageModel = SlideImageModel()
+            slideImageModel.setImage_drawables(myImageList[i])
+            list.add(slideImageModel)
+        }
+
+        return list
+    }
+
+    private fun initImageSlide() {
+        mPager = findViewById(R.id.pager) as ViewPager
+        mPager!!.adapter = SlideImageAdapter(this, this.imageModelArrayList!!)
+
+        val indicator: CirclePageIndicator = findViewById(R.id.indicator)
+
+        indicator.setViewPager(mPager)
+
+        val density = resources.displayMetrics.density
+
+        indicator.setRadius(5 * density)
+        NUM_PAGES = imageModelArrayList!!.size
+
+        val handler = Handler()
+        val Update = Runnable {
+            if (currentPage == NUM_PAGES) {
+                currentPage = 0
+            }
+            mPager!!.setCurrentItem(currentPage++, true)
+        }
+        val swipeTimer = Timer()
+        swipeTimer.schedule(object : TimerTask() {
+            override fun run() {
+                handler.post(Update)
+            }
+        }, 3000, 3000)
+
+        // Pager listener over indicator
+        indicator.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageSelected(position: Int) {
+                currentPage = position
+
+            }
+
+            override fun onPageScrolled(pos: Int, arg1: Float, arg2: Int) {
+
+            }
+
+            override fun onPageScrollStateChanged(pos: Int) {
+
+            }
+        })
     }
 
     override fun onBackPressed() {
@@ -51,10 +122,17 @@ class DashboardActivity : AppCompatActivity() {
         // Handle presses on the action bar menu items
         when (item.itemId) {
             R.id.action_cart -> {
-                Toast.makeText(this,"a",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "a", Toast.LENGTH_LONG).show()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+
+        private var mPager: ViewPager? = null
+        private var currentPage = 0
+        private var NUM_PAGES = 0
     }
 }
